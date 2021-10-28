53f1b06a32dfe33c063a4b50dec8a906db551e68
Thor-ax <maxime.morin593@gmail.com>
2021-10-28 18:02:02 +0200

    Finish to prepare dataset for study

--- clustering.py
+++ clustering.py
@@ -32,7 +32,7 @@ for income in df['income']:
         print("Income of %s = %d indice = %d" %(countries[i], income, i))
     i += 1
 
-# remplaçont les valeurs aberrantes:
+# valeurs aberrantes:
 average_income = df['income'].mean()
 df['income'].replace([80600,75200],average_income, inplace=True)
 
@@ -44,7 +44,7 @@ for expectation in df['life_expectation']:
         print("Life expectation of %f for %s" %(expectation, countries[i]))
     i += 1
 
-# remplaçont les valeurs aberrantes:
+# valeurs aberrantes:
 average_life_expectation = df['life_expectation'].mean()
 df['life_expectation'].replace([0, 32.1],average_life_expectation, inplace=True)
 
@@ -59,13 +59,11 @@ for inflation in df['inflation']:
 average_inflation = df['inflation'].mean()
 df['inflation'].replace(104,average_inflation, inplace=True)
 
-average_gdp = df['GDP'].mean()
+average_gdp = int(df['GDP'].mean())
 df['GDP'].replace(1000000,average_gdp, inplace=True)
 
 def hist():
-    x = np.arange(0, 167)
     for column in columns:
-        data = df[column]
         hist = df[column].hist()
         plt.title(column)
         plt.show()
@@ -81,6 +79,7 @@ def correlation():
 
     plt.show()
     return;
+
 
 X = df['income']
 Y = df['GDP']
@@ -100,27 +99,37 @@ print('Erreur approximation PIB = ', std_err)
 fitLine = predict_PIB(X)
 plt.plot(X, fitLine, c='blue')
 
-# coefficients a et b de la regression linéaire Y = a x + b
-print(slope, intercept)
+def get_index(country):
+    for i,e in enumerate(countries):
+        if e == country:
+            return i
 
+# coefficients a et b de la regression linéaire Y = a x + b
+
+print(get_index('Australia'))
+GDP = df['GDP']
+income = df['income']
+
+print(GDP[get_index('Australia')])
 print('')
-print('PIB Australie = ', predict_PIB(41400))
-print('PIB United Kingdom = ', predict_PIB(36200))
-print('PIB United States = ', predict_PIB(49400))
+print('PIB Australie = ', predict_PIB(income[get_index('Australia')]))
+print('PIB United Kingdom = ', predict_PIB(income[get_index('United Kingdom')]))
+print('PIB United States = ', predict_PIB(income[get_index('United States')]))
 
 #use these values:
-
-df['GDP'][7] = predict_PIB(41400)
-df['GDP'][158] = predict_PIB(36200)
-df['GDP'][159] = predict_PIB(49400)
+print('value = ', )
+df['GDP'][get_index('Australia')] = predict_PIB(income[get_index('Australia')])
+df['GDP'][get_index('United Kingdom')] = predict_PIB(income[get_index('United Kingdom')])
+df['GDP'][get_index('United States')] = predict_PIB(income[get_index('United States')])
 
 #Calculer la valeur manquante du PIB pour l'italie et la Norvège
 
-df['GDP'][75] = predict_PIB(36200)
-df['GDP'][114] = predict_PIB(62300)
+df['GDP'][get_index('Italy')] = predict_PIB(income[get_index('Italy')])
+df['GDP'][get_index('Norway')] = predict_PIB(income[get_index('Norway')])
 
-print("PIB Italie = ", predict_PIB(36200))
-print('PIB Norvège = ', predict_PIB(62300))
+
+print("PIB Italie = ", predict_PIB(income[get_index('Italy')]))
+print('PIB Norvège = ', predict_PIB(income[get_index('Norway')]))
 
 plt.show()
 
@@ -134,13 +143,22 @@ for k in range(len(X)):
     plt.ylabel('Infertilité')
 
 slope, intercept, r_value, p_value, std_err = stats.linregress(X, Y)
-def predict_infertility(x):
+def predict_fertility(x):
    return slope * x + intercept
 
-print('Erreur approximation PIB = ', std_err)
+print(slope, intercept)
+print('')
 
+child_mortality = df['child_mortality']
 
-fitLine = predict_infertility(X)
+print('Erreur approximation Total fertility = ', std_err)
+print('Total fertility France = ', predict_fertility(child_mortality[get_index('France')]))
+print('Total fertility Niger = ', predict_fertility(child_mortality[get_index('Niger')]))
+
+df['total_fertility'][get_index('France')] = predict_fertility(child_mortality[get_index('France')])
+df['total_fertility'][get_index('Niger')] = predict_fertility(child_mortality[get_index('Niger')])
+
+fitLine = predict_fertility(X)
 plt.plot(X, fitLine, c='blue')
 
 plt.show()
